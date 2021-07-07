12ba8e5a424800db4d8ba07900f739a31d09deed
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-07-07 13:52:19 -0400

    add acc token support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from math import floor
 from pathlib import Path
 from pprint import pp
@@ -6,7 +7,8 @@ import time
 from config import OWNER, REPO, INSTALL_ID, APP_ID, PRIVATE_PEM_PATH
 
 CONFIG_FILENAME = 'config.py'
-TOKEN_FILENAME = '.token-info.cfg'
+JWT_FILENAME = '.jwt-token.cfg'
+ACC_TOK_FILENAME = '.acc-token.cfg'
 
 
 def self_check():
@@ -35,7 +37,7 @@ def self_check():
 
 
 def get_jwt(cached: bool = True) -> str:
-    token_f = Path(__file__).parent / TOKEN_FILENAME
+    token_f = Path(__file__).parent / JWT_FILENAME
     token = ''
 
     if cached:
@@ -69,10 +71,6 @@ def get_jwt(cached: bool = True) -> str:
     return token
 
 
-def get_inst_acc_tok() -> str:
-    raise NotImplementedError()
-
-
 def new_sess(jwt: bool = False, acc_tok: bool = False):
     sess = Session()
     sess.headers.clear()
@@ -80,18 +78,53 @@ def new_sess(jwt: bool = False, acc_tok: bool = False):
     if jwt:
         sess.headers['Authorization'] = f'Bearer {get_jwt()}'
     if acc_tok:
-        sess.headers['Authorization'] = f'token  {get_inst_acc_tok()}'
+        sess.headers['Authorization'] = f'token {get_inst_acc_tok()}'
     return sess
+
+
+def get_inst_acc_tok(cached: bool = True) -> str:
+    token_f = Path(__file__).parent / ACC_TOK_FILENAME
+    retry = True
+    token = ''
+
+    if cached:
+        if not token_f.exists():
+            cached = False
+        else:
+            with open(token_f, 'r') as f:
+                exp_time, token = int(
+                    f'0{f.readline().strip()}'), f.readline().strip()
+            cached = time.time() < exp_time and not not token
+
+    if not cached:
+        while True:
+            req = new_sess(jwt=True).post(
+                f'https://api.github.com/app/installations/{INSTALL_ID}/access_tokens'
+            )
+            if retry and req.status_code == 401:
+                get_jwt(cached=False)
+                retry = False
+            else:
+                req.raise_for_status()
+                exp_time = int(
+                    datetime.strptime(req.json()['expires_at'],
+                                      '%Y-%m-%dT%H:%M:%SZ').timestamp())
+                token = req.json()['token']
+                with open(token_f, 'w') as f:
+                    f.write(str(exp_time))
+                    f.write('\n')
+                    f.write(token)
+                break
+
+    return token
 
 
 if __name__ == '__main__':
     self_check()
 
-    req = new_sess(jwt=True).post(
-        'https://api.github.com/app/installations/{INSTALL_ID}/access_tokens')
-    # req = new_sess(acc_tok=True).post(
-    #     f'https://api.github.com/repos/{OWNER}/{REPO}/issues',
-    #     json={'title': 'issue test title'})
+    req = new_sess(acc_tok=True).post(
+        f'https://api.github.com/repos/{OWNER}/{REPO}/issues',
+        json={'title': 'issue test title'})
     pp(f'{req.request.method} {req.request.url}')
     pp(req.status_code)
     pp(req.json())
